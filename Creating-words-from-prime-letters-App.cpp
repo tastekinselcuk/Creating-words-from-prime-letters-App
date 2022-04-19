951f75283b301d54d40b14ed54526fd0d10c449c
Selcuk2534 <77862040+tastekinselcuk@users.noreply.github.com>
2022-04-19 20:57:57 +0300

    Initial commit

---
+++ Creating-words-from-prime-letters-App.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <time.h>
+using namespace std;
+
+int main() {
+    int x, y, d;
+    string z;
+    for (int sayi = 97; sayi <= 123; sayi++) {
+        int kosul = 1;
+        for (int i = 2; i < sayi; i++) {
+            if (sayi % i == 0) {
+                kosul = 0;
+                break;
+            }
+        }
+        if (kosul) {
+            char x = sayi;
+            z += sayi;
+        }
+    }
+    for (int sayi2 = 65; sayi2 <= 91; sayi2++) {
+        int kosul2 = 1;
+        for (int i = 2; i < sayi2; i++) {
+            if (sayi2 % i == 0) {
+                kosul2 = 0;
+                break;
+            }
+        }
+        if (kosul2) {
+            char y = sayi2;
+            z += y;
+        }
+    }
+    srand(time(0));
+    for (int t = 0; t < z.size(); t++) {
+        for (int l = 0; l < 3; l++) {
+            d = rand() % (z.size() - 1) + 0;
+            cout << z[d];
+        }
+        cout << endl;
+    }
+    system("pause");
+    return 0;
+}
